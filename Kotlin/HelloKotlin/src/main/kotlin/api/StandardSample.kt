676b8cbd42f5ed71337d56379ba44ef1ca402156
ztiany <ztiany3@gmail.com>
2017-07-04 11:00:25 +0800

    update kotlin version

--- Kotlin/HelloKotlin/src/main/kotlin/api/StandardSample.kt
+++ Kotlin/HelloKotlin/src/main/kotlin/api/StandardSample.kt
@@ -1,5 +1,7 @@
 package api
 
+import functions.toStr
+
 /**
  *
  * @author Ztiany
@@ -8,9 +10,47 @@ package api
  */
 fun main(args: Array<String>) {
     letSample()
+    applySample()
+    runSample()
+    alsoSample()
+    withSample()
+}
+
+fun withSample() {
+    println("withSample--------------")
+    val result = with(100) {
+        val a = toString()
+        a
+    }
+    println(result)
+}
+
+fun alsoSample() {
+    println("alsoSample--------------")
+    val abc = "ABC".also {
+        println(it)
+    }
+    println(abc)
+}
+
+fun runSample() {
+    println("runSample--------------")
+    println(100.run {
+        println(this)
+        "100 return"
+    })
+}
+
+fun applySample() {
+    println("applySample--------------")
+    val result = 4.apply {
+        println(this)
+    }
+    println(result)
 }
 
 private fun letSample() {
+    println("letSample--------------")
     val abc = 1.let {
         println(it)
         "ABC"
