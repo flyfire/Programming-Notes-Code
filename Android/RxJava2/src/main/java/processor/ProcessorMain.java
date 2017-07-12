b9711ce4b149fa3055bdc084f5ec084447ea908c
ztiany <ztiany3@gmail.com>
2017-07-13 00:25:18 +0800

    add rxjava2 processor

---
+++ Android/RxJava2/src/main/java/processor/ProcessorMain.java
@@ -0,0 +1,56 @@
+package processor;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.processors.BehaviorProcessor;
+
+/**
+ * @author Ztiany
+ *         Email ztiany3@gmail.com
+ *         Date 17.7.13 0:16
+ */
+public class ProcessorMain {
+
+    public static void main(String... args) {
+        testBehaviorProcessor();
+    }
+
+    private static void testBehaviorProcessor() {
+        BehaviorProcessor<String> behaviorProcessor = BehaviorProcessor.create();
+        behaviorProcessor.subscribe(new Subscriber<String>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                s.request(Integer.MAX_VALUE);
+                System.out.println("ProcessorMain.onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println("ProcessorMain.onNext -- " + s);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println("ProcessorMain.onError -- " + t);
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("ProcessorMain.onComplete");
+            }
+        });
+        behaviorProcessor.onNext("A");
+        behaviorProcessor.onNext("B");
+        behaviorProcessor.onNext("C");
+        behaviorProcessor.onNext("D");
+        behaviorProcessor.onError(new RuntimeException("test error"));
+        //之后的订阅者在订阅了发生了错误的behaviorProcessor后，只会受到一个Error事件
+        behaviorProcessor.subscribe(
+                s -> System.out.println("Second onNext -- " + s),
+                throwable -> System.out.println("Second onError -- " + throwable),
+                ()-> System.out.println("Second onComplete")
+        );
+    }
+
+}
