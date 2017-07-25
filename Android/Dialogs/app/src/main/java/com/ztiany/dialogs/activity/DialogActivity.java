5109736620812b79eb4dc8127d3c518b87564725
ztiany <ztiany3@gmail.com>
2017-07-26 00:19:35 +0800

    add henCoder ui

--- Android/Dialogs/app/src/main/java/com/ztiany/dialogs/activity/DialogActivity.java
+++ Android/Dialogs/app/src/main/java/com/ztiany/dialogs/activity/DialogActivity.java
@@ -1,9 +1,14 @@
 package com.ztiany.dialogs.activity;
 
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 
 import com.ztiany.dialogs.R;
@@ -24,6 +29,25 @@ public class DialogActivity extends AppCompatActivity {
         attributes.gravity = Gravity.BOTTOM;
         attributes.width = WindowManager.LayoutParams.MATCH_PARENT;
         getWindow().setAttributes(attributes);
+
+        getWindow().getDecorView().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                View parent = findViewById(R.id.dialog_view);
+                while (parent != null) {
+                    Drawable background = parent.getBackground();
+                    Log.d("DialogActivity", parent + "---" + background);
+                    if (background instanceof ColorDrawable) {
+                        Log.d("DialogActivity", parent + "---" + Integer.toHexString(((ColorDrawable) background).getColor()));
+                    }
+                    if (parent.getParent() != null && parent.getParent() instanceof ViewGroup) {
+                        parent = (ViewGroup) parent.getParent();
+                    } else {
+                        parent = null;
+                    }
+                }
+            }
+        }, 2000);
     }
 
 
