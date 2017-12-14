bb1031177fe098d67a8f398a82e2a336eb430782
ztiany <ztiany3@gmail.com>
2017-12-15 00:37:31 +0800

    c++ function

---
+++ C&C++/cplusplus-program/functions/functions5.cpp
@@ -0,0 +1,39 @@
+/*
+ ============================================================================
+ 
+ Author      : Ztiany
+ Description : 默认参数
+
+ ============================================================================
+ */
+
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+typedef string::size_type sz;
+
+/*参数中可以定义默认参数*/
+static string screen(sz ht = 24, sz wid = 80, char back = ' ');
+
+/*声明内联函数*/
+inline int add(int a, int b) {
+    return a + b;
+}
+
+
+int main() {
+    string window;
+
+    window = screen();
+    window = screen(33);
+    window = screen(33, 44);
+    window = screen(33, 44, '#');
+
+    return EXIT_SUCCESS;
+}
+
+static string screen(sz ht = 24, sz wid = 80, char back = ' ') {
+    return "Fake Screen";
+}
