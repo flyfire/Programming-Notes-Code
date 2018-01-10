a2f37b0a2ad9d0482b8310418b4d0bcca626e830
ztiany <ztiany3@gmail.com>
2018-01-11 00:28:40 +0800

    add cpp notes

---
+++ C&C++/cplusplus-program/memory/DynamicObject.h
@@ -0,0 +1,50 @@
+/*
+ ============================================================================
+ 
+ Author      : Ztiany
+ Description : 
+
+ ============================================================================
+ */
+//
+// Created by Administrator on 18.1.10.
+//
+
+#ifndef CPLUSPLUS_PROGRAM_DYNAMICOBJECT_H
+#define CPLUSPLUS_PROGRAM_DYNAMICOBJECT_H
+
+
+class DynamicObject1 {
+public:
+    DynamicObject1(int a) {
+        mA = a;
+        mB = 100;
+    }
+
+private:
+    int mA;
+    int mB;
+public:
+    int getTotal() {
+        return mA + mB;
+    }
+};
+
+class DynamicObject2 {
+public:
+    DynamicObject2(int a, int b) {
+        mA = a;
+        mB = b;
+    }
+
+private:
+    int mA;
+    int mB;
+public:
+    int getTotal() {
+        return mA + mB;
+    }
+};
+
+
+#endif //CPLUSPLUS_PROGRAM_DYNAMICOBJECT_H
