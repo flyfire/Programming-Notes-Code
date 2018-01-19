abd4fd6139dfc41be4bc28cb9f6d6f5356fbf2ae
ztiany <ztiany3@gmail.com>
2018-01-19 21:59:28 +0800

    cpp note

--- C&C++/cplusplus-program/oo/oo.cpp
+++ C&C++/cplusplus-program/oo/oo.cpp
@@ -13,8 +13,8 @@
 
 using namespace std;
 
-
 int main() {
+
     Sales_item sales_item("cpp primer");
     sales_item.setRevenue(10000);
     sales_item.setUnits_sold(21);
@@ -25,14 +25,12 @@ int main() {
     unsigned int ui = (unsigned int) absObj(i);
     cout << ui << endl;
 
-
     plus<int> intAdd;
     negate<int> intNegate;
     int sum = intAdd(10, 20);
     cout << "sum = " << sum << endl;
     sum = intAdd(10, intNegate(10));
     cout << "sum = " << sum << endl;
-
 
     return EXIT_SUCCESS;
 }
