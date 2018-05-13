219adecac0116474ca01c8319f54d808975e71e6
ztiany <ztiany3@gmail.com>
2018-05-13 23:06:52 +0800

    dev mall project

---
+++ JavaEE/Project-Mall/src/main/java/com/ztiany/mall/service/UserService.java
@@ -0,0 +1,9 @@
+package com.ztiany.mall.service;
+
+/**
+ * @author Ztiany
+ * Email ztiany3@gmail.com
+ * Date 18.5.13 21:54
+ */
+public interface UserService {
+}
---
+++ JavaEE/Project-Mall/src/main/java/com/ztiany/mall/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.ztiany.mall.service.impl;
+
+import com.ztiany.mall.service.UserService;
+
+/**
+ * @author Ztiany
+ * Email ztiany3@gmail.com
+ * Date 18.5.13 23:02
+ */
+public class UserServiceImpl implements UserService {
+}
