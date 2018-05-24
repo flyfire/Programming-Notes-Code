c51de76b5ee528cbd1969cb2ce30fe6947f7fee3
ztiany <ztiany3@gmail.com>
2018-05-24 19:56:55 +0800

    Javassist-Base

---
+++ Java/Javassist-Base/src/main/java/me/ztiany/javassist/Main.java
@@ -0,0 +1,12 @@
+package me.ztiany.javassist;
+
+/**
+ * @author ztiany
+ * Email: ztiany3@gmail.com
+ */
+public class Main {
+
+    public static void main(String... args){
+        System.out.println("Main.main");
+    }
+}
