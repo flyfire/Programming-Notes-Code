9e52a6cca5ee47b924252ad32896db4e109ca137
ztiany <ztiany3@gmail.com>
2018-06-18 23:21:07 +0800

    add chapter04 aop

--- JavaEE/SpringInAction/Chapter03/src/test/java/me/ztiany/test/profile/life/Employee.java
+++
@@ -1,9 +0,0 @@
-package me.ztiany.test.profile.life;
-
-/**
- * @author Ztiany
- * Email ztiany3@gmail.com
- * Date 18.6.14 23:46
- */
-public class Employee {
-}
--- JavaEE/SpringInAction/Chapter03/src/test/java/me/ztiany/test/profile/war/Soldier.java
+++
@@ -1,9 +0,0 @@
-package me.ztiany.test.profile.war;
-
-/**
- * @author Ztiany
- * Email ztiany3@gmail.com
- * Date 18.6.14 23:45
- */
-public class Soldier {
-}
