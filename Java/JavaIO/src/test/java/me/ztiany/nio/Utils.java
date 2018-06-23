14b637256e369762fc97e384317e6187c13ee3cf
ztiany <ztiany3@gmail.com>
2018-06-23 23:22:13 +0800

    add java io sample

---
+++ Java/JavaIO/src/test/java/me/ztiany/nio/Utils.java
@@ -0,0 +1,22 @@
+package me.ztiany.nio;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * @author Ztiany
+ * Email ztiany3@gmail.com
+ * Date 18.6.23 18:46
+ */
+public class Utils {
+
+    public static void close(Closeable fileInputStream) {
+        if (fileInputStream != null) {
+            try {
+                fileInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
