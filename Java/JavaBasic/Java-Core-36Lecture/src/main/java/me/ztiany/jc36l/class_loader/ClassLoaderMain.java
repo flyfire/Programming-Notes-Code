837920f94af8b70a94bfa2fa9327a1b5c37fbe79
ztiany <ztiany3@gmail.com>
2018-07-12 19:59:18 +0800

    add vm sample

---
+++ Java/JavaBasic/Java-Core-36Lecture/src/main/java/me/ztiany/jc36l/class_loader/ClassLoaderMain.java
@@ -0,0 +1,19 @@
+package me.ztiany.jc36l.class_loader;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * @author ztiany
+ * Email: ztiany3@gmail.com
+ */
+public class ClassLoaderMain {
+
+    /*
+    使用参数：-XX:+TraceCla ssLoading   打印加载的类型和卸载的类型的信息
+     */
+    public static void main(String... args) {
+        /* 使用 java agent
+        Instrumentation instrumentation;
+        instrumentation.getAllLoadedClasses()*/
+    }
+}
