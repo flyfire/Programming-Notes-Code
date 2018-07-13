b243d3e4b277fc0b017b81c65db9d331932f57f1
ztiany <ztiany3@gmail.com>
2018-07-13 18:39:39 +0800

    add jvm code

--- Java/JavaBasic/Java-Core-36Lecture/src/main/java/me/ztiany/jc36l/class_loader/ClassLoaderMain.java
+++ Java/JavaBasic/Java-Core-36Lecture/src/main/java/me/ztiany/jc36l/class_loader/ClassLoaderMain.java
@@ -9,11 +9,12 @@ import java.lang.instrument.Instrumentation;
 public class ClassLoaderMain {
 
     /*
-    使用参数：-XX:+TraceCla ssLoading   打印加载的类型和卸载的类型的信息
+    使用参数：-XX:+TraceClassLoading -XX:+TraceClassUnloading  打印加载的类型和卸载的类型的信息
      */
     public static void main(String... args) {
         /* 使用 java agent
         Instrumentation instrumentation;
         instrumentation.getAllLoadedClasses()*/
+        System.out.println("hha");
     }
 }
