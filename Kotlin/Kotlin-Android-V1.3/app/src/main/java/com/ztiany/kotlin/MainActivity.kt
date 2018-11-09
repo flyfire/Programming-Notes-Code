f2375d9244fca2f6556e4c725a50931ce3fa5342
Zhanty <Ztiany3@gmail.com>
2018-11-09 18:06:35 +0800

    add ktx

--- Kotlin/Kotlin-Android-V1.3/app/src/main/java/com/ztiany/kotlin/MainActivity.kt
+++ Kotlin/Kotlin-Android-V1.3/app/src/main/java/com/ztiany/kotlin/MainActivity.kt
@@ -10,8 +10,10 @@ import com.ztiany.kotlin.anko.coroutines.AnkoCoroutinesActivity
 import com.ztiany.kotlin.anko.layouts.AnkoLayoutsActivity
 import com.ztiany.kotlin.coroutines.CoroutinesActivity
 import com.ztiany.kotlin.extension.ExtensionSampleActivity
+import com.ztiany.kotlin.ktx.KtxActivity
 import org.jetbrains.anko.button
 import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.scrollView
 import org.jetbrains.anko.verticalLayout
 
 
@@ -20,52 +22,61 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        verticalLayout {
+        scrollView {
 
-            button("Android Extension 插件") {
-                setOnClickListener {
-                    startActivity(intentFor<ExtensionSampleActivity>())
-                }
-            }
-            button("Android 协程示例") {
-                setOnClickListener {
-                    startActivity(intentFor<CoroutinesActivity>())
-                }
-            }
+            verticalLayout {
 
-            button("Anko Common") {
-                setOnClickListener {
-                    startActivity(intentFor<AnkoCommonActivity>())
+                button("Android Extension 插件") {
+                    setOnClickListener {
+                        startActivity(intentFor<ExtensionSampleActivity>())
+                    }
                 }
-            }
-
-            button("Anko Common Intents") {
-                setOnClickListener {
-                    startActivity(intentFor<RawActivity>())
+                button("Android 协程示例") {
+                    setOnClickListener {
+                        startActivity(intentFor<CoroutinesActivity>())
+                    }
                 }
-            }
 
-            button("Anko Common Dialogs") {
-                setOnClickListener {
-                    startActivity(intentFor<DialogsActivity>())
+                button("Anko Common") {
+                    setOnClickListener {
+                        startActivity(intentFor<AnkoCommonActivity>())
+                    }
                 }
-            }
 
-            button("Anko Common Logger") {
-                setOnClickListener {
-                    startActivity(intentFor<LoggerActivity>())
+                button("Anko Common Intents") {
+                    setOnClickListener {
+                        startActivity(intentFor<RawActivity>())
+                    }
                 }
-            }
 
-            button("Anko Layouts") {
-                setOnClickListener {
-                    startActivity(intentFor<AnkoLayoutsActivity>())
+                button("Anko Common Dialogs") {
+                    setOnClickListener {
+                        startActivity(intentFor<DialogsActivity>())
+                    }
                 }
-            }
 
-            button("Anko Coroutines") {
-                setOnClickListener {
-                    startActivity(intentFor<AnkoCoroutinesActivity>())
+                button("Anko Common Logger") {
+                    setOnClickListener {
+                        startActivity(intentFor<LoggerActivity>())
+                    }
+                }
+
+                button("Anko Layouts") {
+                    setOnClickListener {
+                        startActivity(intentFor<AnkoLayoutsActivity>())
+                    }
+                }
+
+                button("Anko Coroutines") {
+                    setOnClickListener {
+                        startActivity(intentFor<AnkoCoroutinesActivity>())
+                    }
+                }
+
+                button("KtxActivity") {
+                    setOnClickListener {
+                        startActivity(intentFor<KtxActivity>())
+                    }
                 }
             }
         }
