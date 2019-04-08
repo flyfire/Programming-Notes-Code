bbd9831891432919e10e0a535bf858a83b1bc0e5
ztiany <ztiany3@gmail.com>
2019-04-09 00:25:22 +0800

    update front-end

--- WeChat/HelloWorld/pages/index/index.js
+++ WeChat/HelloWorld/pages/index/index.js
@@ -3,21 +3,18 @@
 const app = getApp()
 
 Page({
-
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
@@ -46,7 +43,6 @@ Page({
       })
     }
   },
-
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
